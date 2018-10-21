966d5360bc1c70529139c49dafb37cca171f6bb0
reguv760 <reguv760@gmail.com>
2018-10-20 22:24:44 -0700

    google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,20 @@ module.exports = {
       }
     },    
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-6371690-5",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
