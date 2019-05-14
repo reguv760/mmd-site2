5fb0d97059a8cbe7cac8b1b8d3d0891f962bd48c
reguv760 <reguv760@gmail.com>
2019-05-14 15:02:21 -0700

    add public art to mobileMenu

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,109 +3,104 @@ import PropTypes from 'prop-types';
 import Lightbox from 'react-images';
 
 class Gallery extends React.Component {
-    constructor () {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
-        };
+    this.state = {
+      lightboxIsOpen: false,
+      currentImage: 0
+    };
 
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.gotoImage = this.gotoImage.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-    }
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    gotoImage (index) {
-        this.setState({
-            currentImage: index,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.gotoImage = this.gotoImage.bind(this);
+    this.handleClickImage = this.handleClickImage.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+  }
+  openLightbox(index, event) {
+    event.preventDefault();
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1
+    });
+  }
+  gotoImage(index) {
+    this.setState({
+      currentImage: index
+    });
+  }
+  handleClickImage() {
+    if (this.state.currentImage === this.props.images.length - 1) return;
 
-        this.gotoNext();
-    }
-    renderGallery () {
-        const { images } = this.props;
+    this.gotoNext();
+  }
+  renderGallery() {
+    const { images } = this.props;
 
-        if (!images) return;
+    if (!images) return;
 
+    /* {  this is output of menu items  } */
+    const gallery = images.map((obj, i) => {
+      return (
+        <li className="gallery-thumbContainer" key={i}>
+          <a
+            className="gallery-thumb"
+            href={obj.src}
+            onClick={e => this.openLightbox(i, e)}
+          >
+            <img src={obj.thumbnail} alt="thumbnail" />
+          </a>
 
-        /* {  this is output of menu items  } */
-        const gallery = images.map((obj, i) => {
-            return (
-                <li className="gallery-thumbContainer" key={i}>
-                    <a
-                        className="gallery-thumb"
-                        href={obj.src}
-                        onClick={(e) => this.openLightbox(i, e)}
-                    >
-                        <img src={obj.thumbnail} alt="thumbnail" />
-                    </a>
+          {/* <h3>{obj.caption}</h3>
+                    <p>{obj.description}</p> */}
+        </li>
+      );
+    });
 
-                    { /* <h3>{obj.caption}</h3>
-                    <p>{obj.description}</p> */ }
-                </li>
-            );
-        });
+    /* {  this is the main output of Gallery.js!  } */
+    return <ul className="galleryContainer">{gallery}</ul>;
+  }
 
-        /* {  this is the main output of Gallery.js!  } */
-        return (
-            <ul className="galleryContainer">
-                {gallery}
-            </ul>
-        );
-    }
-
-    //this is the output of this class:::
-    render () {
-        return (
-            <div>
-                {this.renderGallery()}
-                <Lightbox
-                    currentImage={this.state.currentImage}
-                    images={this.props.images}
-                    isOpen={this.state.lightboxIsOpen}
-                    onClickImage={this.handleClickImage}
-                    onClickNext={this.gotoNext}
-                    onClickPrev={this.gotoPrevious}
-                    onClickThumbnail={this.gotoImage}
-                    onClose={this.closeLightbox}
-                />
-            </div>
-        );
-    }
+  //this is the output of this class:::
+  render() {
+    return (
+      <div>
+        {this.renderGallery()}
+        <Lightbox
+          currentImage={this.state.currentImage}
+          images={this.props.images}
+          isOpen={this.state.lightboxIsOpen}
+          onClickImage={this.handleClickImage}
+          onClickNext={this.gotoNext}
+          onClickPrev={this.gotoPrevious}
+          onClickThumbnail={this.gotoImage}
+          onClose={this.closeLightbox}
+        />
+      </div>
+    );
+  }
 }
 
 Gallery.displayName = 'Gallery';
 Gallery.propTypes = {
-    images: PropTypes.array
+  images: PropTypes.array
 };
 
 export default Gallery;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
 
 //component import
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
 
 //import SEO from './SEO';
-import './../style/layout.scss'
+import './../style/layout.scss';
 
 /* this is the parent index file that 
 renders main "app" page */
@@ -34,18 +34,18 @@ const Layout = ({ children }) => (
           meta={[
             {
               name: 'description',
-              content: `${data.site.siteMetadata.description}`,
+              content: `${data.site.siteMetadata.description}`
             },
             {
               name: 'keywords',
-              content: `${data.site.siteMetadata.keywords}`,
+              content: `${data.site.siteMetadata.keywords}`
             },
-            { name: 'author', content: `${data.site.siteMetadata.siteAuthor}` },
+            { name: 'author', content: `${data.site.siteMetadata.siteAuthor}` }
           ]}
           htmlAttributes={{ lang: 'en' }}
         />
 
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header />
 
         <div className="container">{children}</div>
 
@@ -53,10 +53,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
