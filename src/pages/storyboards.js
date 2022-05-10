e03b28f8791caecb550ae64b969cc5dff2600b1b
Reggie Galang <reguv760@gmail.com>
2022-05-10 08:55:58 -0700

    clean up pages with gallery; update global orange and off-white + dropdown alpha

--- src/pages/storyboards.js
+++ src/pages/storyboards.js
@@ -44,49 +44,49 @@ const VREAD_IMAGES = [
 ];
 
 export default class StoryboardPage extends React.Component {
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.state = {
-      lightboxIsOpen: false,
-      currentImage: 0
-    };
+  //   this.state = {
+  //     lightboxIsOpen: false,
+  //     currentImage: 0
+  //   };
 
-    this.closeLightbox = this.closeLightbox.bind(this);
-    this.gotoNext = this.gotoNext.bind(this);
-    this.gotoPrevious = this.gotoPrevious.bind(this);
-    this.openLightbox = this.openLightbox.bind(this);
-    this.handleClickImage = this.handleClickImage.bind(this);
-  }
+  //   this.closeLightbox = this.closeLightbox.bind(this);
+  //   this.gotoNext = this.gotoNext.bind(this);
+  //   this.gotoPrevious = this.gotoPrevious.bind(this);
+  //   this.openLightbox = this.openLightbox.bind(this);
+  //   this.handleClickImage = this.handleClickImage.bind(this);
+  // }
 
-  openLightbox(index, event) {
-    event.preventDefault();
-    this.setState({
-      currentImage: index,
-      lightboxIsOpen: true
-    });
-  }
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false
-    });
-  }
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1
-    });
-  }
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1
-    });
-  }
-  handleClickImage() {
-    if (this.state.currentImage === this.props.images.length - 1) return;
+  // openLightbox(index, event) {
+  //   event.preventDefault();
+  //   this.setState({
+  //     currentImage: index,
+  //     lightboxIsOpen: true
+  //   });
+  // }
+  // closeLightbox() {
+  //   this.setState({
+  //     currentImage: 0,
+  //     lightboxIsOpen: false
+  //   });
+  // }
+  // gotoPrevious() {
+  //   this.setState({
+  //     currentImage: this.state.currentImage - 1
+  //   });
+  // }
+  // gotoNext() {
+  //   this.setState({
+  //     currentImage: this.state.currentImage + 1
+  //   });
+  // }
+  // handleClickImage() {
+  //   if (this.state.currentImage === this.props.images.length - 1) return;
 
-    this.gotoNext();
-  }
+  //   this.gotoNext();
+  // }
 
   render() {
     const { title } = this.props.data.site.siteMetadata;
