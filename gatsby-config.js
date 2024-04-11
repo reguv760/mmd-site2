8c20612fe7c2a7f57e7011b9aa3a9d95b3c1c61c
Reggie Galang <reguv760@gmail.com>
2024-04-10 19:10:06 -0700

    add fade-in animation to .dropdown__content

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     description:
       'Wet Paint(n.) A state of freshness and vigor that categorizes the art of Melany Meza-Dierks.',
     keywords:
-      'Fine Artist, Los Angeles, Long Beach, California, Caracas, Venezuela, Tattoos, Advertising, Graphic Design, Art Director, Teacher, Mural Painter',
+      'Fine Artist, Los Angeles, Long Beach, California, Caracas, Venezuela, Tattoos, Advertising, Graphic Design, Art Gallery, Art Director, Teacher, Mural Painter',
     siteAuthor: 'Reginald Galang',
     url: 'http://mywetpaintstudio.com',
     siteUrl: 'http://mywetpaintstudio.com'
