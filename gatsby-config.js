8f7a9b515d0bf22f25c989bb2c96115eec95cf2d
Reggie Galang <reguv760@gmail.com>
2024-04-10 18:50:58 -0700

    replaced gatsby-plugin-google-analytics with gatsby-plugin-google-gtag analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,13 +42,36 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: 'UA-6371690-5',
+    //     // Puts tracking script in the head instead of the body
+    //     head: true
+    //   }
+    // }
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-6371690-5',
-        // Puts tracking script in the head instead of the body
-        head: true
-      }
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-6371690-5"
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
     }
   ]
 };
