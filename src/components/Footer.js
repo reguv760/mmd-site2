18bee08234f5b19172734c49ac56f5be40590b5b
Reggie Galang <reguv760@gmail.com>
2024-05-13 14:41:07 -0700

    changed Footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,7 +34,7 @@ const Footer = () => (
         />
       </li>
 
-      <li className="year">&copy; {currentDate.getFullYear()} Melany Dierks</li>
+      <li className="year">&copy; {currentDate.getFullYear()} Melany MD</li>
       <li className="webdev">
         website by:{' '}
         <a
