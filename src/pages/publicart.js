b40ae5754b66c70e79eacbcb88e5157a58452bc9
Reggie Galang <reguv760@gmail.com>
2026-07-01 14:43:46 -0700

    add new images artwork + captions to public art images

--- src/pages/publicart.js
+++ src/pages/publicart.js
@@ -111,68 +111,326 @@ import MURALS2025_2 from './../images/publicart/publicArt-25_2.jpg';
 import MURALS2025_3 from './../images/publicart/publicArt-25_3.jpg';
 import MURALS2025_4 from './../images/publicart/publicArt-25_4.jpg';
 
+//2026
+import MURALS26_thumb1 from './../images/publicart/2026/thumbs/mural1-thumb.jpg';
+import MURALS26_thumb2 from './../images/publicart/2026/thumbs/mural2-thumb.jpg';
+import MURALS26_thumb3 from './../images/publicart/2026/thumbs/mural3-thumb.jpg';
+import MURALS26_thumb4 from './../images/publicart/2026/thumbs/mural4-thumb.jpg';
+import MURALS26_thumb5 from './../images/publicart/2026/thumbs/mural5-thumb.jpg';
+import MURALS26_thumb6 from './../images/publicart/2026/thumbs/mural6-thumb.jpg';
+import MURALS26_thumb7 from './../images/publicart/2026/thumbs/mural7-thumb.jpg';
+import MURALS26_thumb8 from './../images/publicart/2026/thumbs/mural8-thumb.jpg';
+
+
+import MURALS2026_1 from './../images/publicart/2026/mural-1.jpg';
+import MURALS2026_2 from './../images/publicart/2026/mural-2.jpg';
+import MURALS2026_3 from './../images/publicart/2026/mural-3.jpg';
+import MURALS2026_4 from './../images/publicart/2026/mural-4.jpg';
+import MURALS2026_5 from './../images/publicart/2026/mural-5.jpg';
+import MURALS2026_6 from './../images/publicart/2026/mural-6.jpg';
+import MURALS2026_7 from './../images/publicart/2026/mural-7.jpg';
+import MURALS2026_8 from './../images/publicart/2026/mural-8.jpg';
+
+
+
+const PUBLICART_26 = [
+  { 
+    id: '0', 
+    src: MURALS2026_1, 
+    thumbnail: MURALS26_thumb1,
+    caption: `private residence, Lakewood | 2022`
+  },
+  { 
+    id: '1', 
+    src: MURALS2026_2, 
+    thumbnail: MURALS26_thumb2,
+    caption: ``
+  },
+  { 
+    id: '2', 
+    src: MURALS2026_3, 
+    thumbnail: MURALS26_thumb3,
+    caption: `LA landscape | private residence, Simi Valley, CA | 07/2025`
+  },
+  { 
+    id: '3', 
+    src: MURALS2026_4, 
+    thumbnail: MURALS26_thumb4,
+    caption: `Dog Sandworm | private residence | Long Beach, CA | 10/2024`
+  },
+  { 
+    id: '4', 
+    src: MURALS2026_5, 
+    thumbnail: MURALS26_thumb5,
+    caption: `Nursery Nebula | private residence, Wilmington, CA | 09/2025`
+  },
+  { 
+    id: '5', 
+    src: MURALS2026_6, 
+    thumbnail: MURALS26_thumb6,
+    caption: `private residence, Winnetka, CA | 2021`
+  },
+  { 
+    id: '6', 
+    src: MURALS2026_7, 
+    thumbnail: MURALS26_thumb7,
+    caption: `private residence, Winnetka, CA | 2021`
+  },
+  { 
+    id: '7', 
+    src: MURALS2026_8, 
+    thumbnail: MURALS26_thumb8,
+    caption: `private residence, Lakewood, CA | 2022`
+  },
+];
+
 
 const PUBLICART_23 = [
-  { id: '0', src: Full1_23, thumbnail: Thumb1_23 },
-  { id: '1', src: Full2_23, thumbnail: Thumb2_23 },
+  { id: '0', 
+    src: Full1_23, 
+    thumbnail: Thumb1_23,
+    caption: `Roland Sands Designs | 2022` 
+  },
+  { 
+    id: '1',
+    src: Full2_23, 
+    thumbnail: Thumb2_23,
+    caption: `private residence, Glendale, CA | 2023` 
+  },
 ];
 
 
 const PUBLICART_IMAGES = [
-  { id: '0', src: BBLegends, thumbnail: BBLegendsThumb },
-  { id: '1', src: Ruth, thumbnail: RuthThumb },
-  { id: '2', src: Dragon, thumbnail: DragonThumb },
-  { id: '3', src: paFull03, thumbnail: paThumb03 },
-  { id: '4', src: paFull02, thumbnail: paThumb02 },
-  { id: '5', src: paFull01, thumbnail: paThumb01 }
+  { 
+    id: '0', 
+    src: BBLegends,
+    thumbnail: BBLegendsThumb,
+    caption: `Kaptain Fish | 2022`
+  },
+  { 
+    id: '1',
+    src: Ruth,
+    thumbnail: RuthThumb,  
+    caption: `` },
+  { 
+    id: '2', 
+    src: Dragon, 
+    thumbnail: DragonThumb,  
+    caption: `Quetzalcoatl | 3rd Street Market Tacos & Beer - 4600 3rd street, CA 90022 | 2020` 
+  },
+  { 
+    id: '3', 
+    src: paFull03, 
+    thumbnail: paThumb03,    
+    caption: `private residence Lakewood, CA | 04/2019` 
+  },
+  { 
+    id: '4', 
+    src: paFull02, 
+    thumbnail: paThumb02,  
+    caption: `private residence Lakewood, CA | 04/2019`
+  },
+  { 
+    id: '5', 
+    src: paFull01, 
+    thumbnail: paThumb01,
+    caption: `Pier 76 fish grill - Cerritos, CA | 04/2019`
+  }
 ];
 
 const OCTOPUS_IMAGES = [
-  { id: '0', src: octFull3, thumbnail: octThumb3 },
-  { id: '1', src: octFull2, thumbnail: octThumb2 },
-  { id: '2', src: octFull1, thumbnail: octThumb1 },
+  { 
+    id: '0', 
+    src: octFull3,
+    thumbnail: octThumb3,
+    caption: `Crafty Minds Brews and Bites - Hermosa, CA | 05/2019` 
+  },
+  { id: '1',
+    src: octFull2, 
+    thumbnail: octThumb2,
+    caption: `Crafty Minds Brews and Bites - Hermosa, CA | 05/2019` 
+  },
+  { 
+    id: '2', 
+    src: octFull1,
+    thumbnail: octThumb1, 
+    caption: `Crafty Minds Brews and Bites - Hermosa, CA | 05/2019` 
+  }
 ];
 
 const KOBENUT_IMAGES = [
-  { id: '0', src: KobeFull4, thumbnail: KobeThumb4 },
-  { id: '1', src: KobeFull2, thumbnail: KobeThumb2 },
-  { id: '2', src: KobeFull3, thumbnail: KobeThumb3 },
-  { id: '3', src: KobeFull1, thumbnail: KobeThumb1 },
-  { id: '4', src: KobeFull5, thumbnail: KobeThumb5 },
-  { id: '5', src: KobeFull6, thumbnail: KobeThumb6},
-  { id: '6', src: KobeFull7, thumbnail: KobeThumb7 },
-  { id: '7', src: KobeFull8, thumbnail: KobeThumb8 }
+  { 
+    id: '0',
+    src: KobeFull4, 
+    thumbnail: KobeThumb4,
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  },
+  { 
+    id: '1',
+    src: KobeFull2, 
+    thumbnail: KobeThumb2,  
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  },
+  { 
+    id: '2', 
+    src: KobeFull3,
+    thumbnail: KobeThumb3,  
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  },
+  { 
+    id: '3', 
+    src: KobeFull1, 
+    thumbnail: KobeThumb1, 
+    caption: `Superior Nut Co - Los Angeles, CA | 2020` 
+  },
+  { 
+    id: '4', 
+    src: KobeFull5,
+    thumbnail: KobeThumb5,
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  },
+  { 
+    id: '5', 
+    src: KobeFull6, 
+    thumbnail: KobeThumb6,
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  },
+  { 
+    id: '6', 
+    src: KobeFull7, 
+    thumbnail: KobeThumb7, 
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  },
+  { 
+    id: '7', 
+    src: KobeFull8, 
+    thumbnail: KobeThumb8,
+    caption: `Superior Nut Co - Los Angeles, CA | 2020`
+  }
 ];
 
 const MERMAID_IMAGES = [
-  { id: '0', src: MerMaid1, thumbnail: MermaidThumb1 },
-  { id: '1', src: MerMaid2, thumbnail: MermaidThumb2 },
+  { 
+    id: '0', 
+    src: MerMaid1, 
+    thumbnail: MermaidThumb1,
+    caption: `Ocean Caves | Roe Seafood - Long Beach, CA | 06/2019` 
+  },
+  { 
+    id: '1', 
+    src: MerMaid2,
+    thumbnail: MermaidThumb2,  
+    caption: `Ocean Caves | Roe Seafood - Long Beach, CA | 06/2019` 
+  }
 ];
 
 const LEGENDS_IMAGES = [
-  { id: '0', src: Legends1, thumbnail: LegendsThumb1 },
-  { id: '1', src: Legends2, thumbnail: LegendsThumb2 },
-  { id: '2', src: Legends3, thumbnail: LegendsThumb3 },
-  { id: '3', src: Legends8, thumbnail: LegendsThumb8 },
-  { id: '4', src: Legends5, thumbnail: LegendsThumb5 },
-  { id: '5', src: Legends4, thumbnail: LegendsThumb4 },
-  { id: '6', src: Legends6, thumbnail: LegendsThumb6 },
-  { id: '7', src: Legends7, thumbnail: LegendsThumb7 },
-  { id: '8', src: Legends9, thumbnail: LegendsThumb9 },
-  { id: '9', src: Legends10, thumbnail: LegendsThumb10 }
+  { 
+    id: '0', 
+    src: Legends1, 
+    thumbnail: LegendsThumb1,
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  },
+  { 
+    id: '1',
+    src: Legends2, 
+    thumbnail: LegendsThumb2,
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  },
+  { 
+    id: '2', 
+    src: Legends3, 
+    thumbnail: LegendsThumb3, 
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  },
+  { 
+    id: '3',
+    src: Legends8,
+    thumbnail: LegendsThumb8,  
+    caption: `Superior Nut Co - Los Angeles, CA | 2023` 
+  },
+  { 
+    id: '4', 
+    src: Legends5,
+    thumbnail: LegendsThumb5,  
+    caption: `Superior Nut Co - Los Angeles, CA | 2023` 
+  },
+  { 
+    id: '5',
+    src: Legends4, 
+    thumbnail: LegendsThumb4,  
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  },
+  {
+    id: '6', 
+    src: Legends6, 
+    thumbnail: LegendsThumb6,
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  },
+  { 
+    id: '7', 
+    src: Legends7, 
+    thumbnail: LegendsThumb7, 
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  },
+  {
+    id: '8', 
+    src: Legends9, 
+    thumbnail: LegendsThumb9,  
+    caption: `Superior Nut Co - Los Angeles, CA | 2023` 
+  },
+  { 
+    id: '9', 
+    src: Legends10, 
+    thumbnail: LegendsThumb10,
+    caption: `Superior Nut Co - Los Angeles, CA | 2023`
+  }
 ];
 
 const EUPHOROS_IMAGES = [
-  { id: '0', src: euphorosFull1, thumbnail: euphorosThumb1 },
-  { id: '1', src: euphorosFull2, thumbnail: euphorosThumb2 },
-  { id: '2', src: euphorosFull3, thumbnail: euphorosThumb3 },
+  { 
+    id: '0',
+    src: euphorosFull1, 
+    thumbnail: euphorosThumb1,
+    caption: `Doni Kobe mural | Euphoros Dispensary - 2934 N. Broadway Los Angeles 90031 | 03/2022`
+  },
+  { id: '1', 
+    src: euphorosFull2, 
+    thumbnail: euphorosThumb2,
+    caption: `Doni Kobe mural | Euphoros Dispensary - 2934 N. Broadway Los Angeles 90031 | 03/2022`
+  },
+  { 
+    id: '2', 
+    src: euphorosFull3, 
+    thumbnail: euphorosThumb3,
+    caption: `Doni Kobe mural | Euphoros Dispensary - 2934 N. Broadway Los Angeles 90031 | 03/2022`
+  },
 ];
 
 const MURALS_25 = [
-  { id: '0', src: MURALS2025_1, thumbnail: MURALS25_thumb1 },
-  { id: '1', src: MURALS2025_2, thumbnail: MURALS25_thumb2 },
-  { id: '2', src: MURALS2025_3, thumbnail: MURALS25_thumb3 },
-  { id: '3', src: MURALS2025_4, thumbnail: MURALS25_thumb4 },
+  { 
+    id: '0', 
+    src: MURALS2025_1, 
+    thumbnail: MURALS25_thumb1,
+    caption: `Kobe + Freddy Freeman | private residence, Los Angeles, CA | 04/2025`
+  },
+  { 
+    id: '1', 
+    src: MURALS2025_2, 
+    thumbnail: MURALS25_thumb2,
+    caption: `starry portrait | private residence, Long Beach, CA | 02/2025`
+  },
+  { 
+    id: '2', 
+    src: MURALS2025_3, 
+    thumbnail: MURALS25_thumb3,
+    caption: `youth and birds (memorial) | private residence, Lakewood, CA | 05/2025` 
+  },
+  { 
+    id: '3', 
+    src: MURALS2025_4, 
+    thumbnail: MURALS25_thumb4,  
+    caption: `Rodney Dangerfield portrait | 4481 35th St, San Diego, CA | 05/2025` },
 ];
 
 export default class PublicArtPage extends React.Component {
@@ -193,67 +451,83 @@ export default class PublicArtPage extends React.Component {
           <div className="pageContainer right">           
 
             <Gallery
-              images={MURALS_25.map(({ id, src, thumbnail }) => ({
+              images={PUBLICART_26.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
-              }))}
-            />
-            
-
-            <Gallery
-              images={PUBLICART_23.map(({ id, src, thumbnail }) => ({
-                id,
-                src,
-                thumbnail
+                thumbnail,
+                caption
               }))}
             />
 
             <Gallery
-              images={PUBLICART_IMAGES.map(({ id, src, thumbnail }) => ({
+              images={MURALS_25.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
+                thumbnail,
+                caption
               }))}
             />
 
             <Gallery
-              images={KOBENUT_IMAGES.map(({ id, src, thumbnail }) => ({
+              images={PUBLICART_IMAGES.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
+                thumbnail,
+                caption
               }))}
             />
 
             <Gallery
-              images={LEGENDS_IMAGES.map(({ id, src, thumbnail }) => ({
+              images={KOBENUT_IMAGES.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
+                thumbnail,
+                caption
+              }))}
+            />
+
+            <Gallery
+              images={LEGENDS_IMAGES.map(({ id, src, thumbnail, caption }) => ({
+                id,
+                src,
+                thumbnail,
+                caption
               }))}
             />                       
             
             <Gallery
-              images={EUPHOROS_IMAGES.map(({ id, src, thumbnail }) => ({
+              images={EUPHOROS_IMAGES.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
+                thumbnail,
+                caption
               }))}
             />
 
             <Gallery
-              images={OCTOPUS_IMAGES.map(({ id, src, thumbnail }) => ({
+              images={OCTOPUS_IMAGES.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
+                thumbnail,
+                caption
+              }))}
+            />       
+
+            <Gallery
+              images={PUBLICART_23.map(({ id, src, thumbnail, caption }) => ({
+                id,
+                src,
+                thumbnail,
+                caption
               }))}
             />
 
             <Gallery
-              images={MERMAID_IMAGES.map(({ id, src, thumbnail }) => ({
+              images={MERMAID_IMAGES.map(({ id, src, thumbnail, caption }) => ({
                 id,
                 src,
-                thumbnail
+                thumbnail,
+                caption
               }))}
             />
 
